530522a4edd91e6583a4b0a6229425ff924304b6
Johyechan <johyechan6@gmail.com>
2025-02-11 18:35:15 +0900

    Add Null Check
    as 연산자에서 null을 반환하는 예외 상황을 대비하여 null체크를 추가했습니다.

--- Design Pattern/Assets/02 Scripts/SingletonPattern/Manager/Singleton.cs
+++ Design Pattern/Assets/02 Scripts/SingletonPattern/Manager/Singleton.cs
@@ -15,13 +15,13 @@ namespace Manager
                 // 만약 instance가 null일 경우 (싱글톤 인스턴스가 아직 없음)
                 if (instance == null)
                 {
-                    // 현재 씬에서 T 타입인 오브젝트를 찾아 instance에 할당
+                    // 현재 씬에서 T 타입인 첫 번째 오브젝트를 찾아 instance에 할당
                     instance = FindFirstObjectByType<T>();
 
                     // T와 동일한 타입인 Object를 못 찾은 경우
                     if (instance == null)
                     {
-                        // 새로운 GameObject를 생성(이름은 클래스의 이름을 따름)
+                        // 새로운 GameObject를 생성(이름은 클래스의 이름을 따름) *typeof()는 클래스, 구조체, 인터페이스 등의 타입의 정보를 가져오는 연산자
                         GameObject obj = new GameObject(typeof(T).Name);
                         // 그리고 instance는 GameObject에 추가하는 T 타입의 컴포넌트로 할당
                         instance = obj.AddComponent<T>();
@@ -39,10 +39,17 @@ namespace Manager
             // 현재 싱글톤 인스턴스가 null이라면 이 오브젝트를 싱글톤으로 설정
             if (instance == null)
             {
-                instance = this as T;
-                DontDestroyOnLoad(gameObject);
+                instance = this as T; // *as 연산자는 안전한 형 변환을 수행함 그래서 변환이 가능하면 해당 타입으로 불가능 하면 null을 반환함 (주의 사항으로는 값 타입(struct)에는 사용할 수 없고 참조 타입(class)에만 적용가능함, 변환 실패시 null 반환함으로 null 체크를 해야한다)
+                if(instance != null)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+                else
+                {
+                    Debug.LogWarning($"{typeof(T)} 형태로 변환 불가능, 올바른 싱글톤이 아님");
+                }
             }
-            else if (instance != this)
+            else if (instance != this) // *이미 존재하는 인스턴스와 새로 생성된 오브젝트가 다르다 = 중복된 객체가 생성된 것이므로 새로 생성된 this를 삭제
             {
                 // 만약 이미 존재하는 싱글톤 인스턴스가 있다면, 새로 생성된 이 중복 오브젝트는 삭제
                 Destroy(gameObject);
